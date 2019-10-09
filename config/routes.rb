25fdfeec513a1258a1ba90540fb552d15329dbcf
Mark Francis <markxf@gmail.com>
2019-10-09 18:46:28 +0100

    migration for user post reference

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about', as: :about
   get 'terms', to: 'pages#terms', as: :terms
   get 'privacy', to: 'pages#privacy', as: :privacy
+  resources :posts
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
